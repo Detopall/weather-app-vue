136c8f0a5181b37a69b84a846318d382e8a7e98b
denis.topallaj <denis.topallaj13@gmail.com>
2022-10-29 12:42:05 +0200

    the weather app is finished, error handeling is done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-/* 90afc290a4msh942c639bb0fe563p15afe0jsn7d560927cf10 */
+
 const _firstCountry = 0;
 const URL = "https://foreca-weather.p.rapidapi.com";
 const _weatherApi = "foreca-weather.p.rapidapi.com";
@@ -11,53 +11,57 @@ const _app = Vue.createApp({
 			currentCode: listOfCountries[_firstCountry].code,
 			currentName: listOfCountries[_firstCountry].name,
 			resultInfoCountry: null,
-			weatherInformation: null,
+			noError: true,
 		}
 	},
+
 	methods: {
 		changeCountry(e){
 			this.currentCode = e.target.value;
 			this.currentName = this.countries.filter(c => c.code === e.target.value)[_firstCountry].name;
 		},
-		async processCountryInfoRequest(){
-			const options = {
+
+		optionsForApi(){
+			return {
 				method: 'GET',
 				headers: {
 					'X-RapidAPI-Key': this.$refs.rapidAPIKey.value,
 					'X-RapidAPI-Host': _weatherApi,
 				}
 			};
+		},
+
+		async processCountryInfoRequest(){
+			const options = this.optionsForApi();
 			
-			this.resultInfoCountry = await fetch(`${URL}/location/search/${this.$refs.city.value}?lang=en&country=${this.currentCode.toLowerCase()}`, options)
-			.then(res => res.json());
-			this.weatherInformation = await this.processWeatherRequest();
+			const fetchedCountryInfo = await fetch(`${URL}/location/search/${this.$refs.city.value}?lang=en&country=${this.currentCode.toLowerCase()}`, options);
+			
+			this.resultInfoCountry = await fetchedCountryInfo.json();
+
+			if (this.resultInfoCountry.locations[0] === undefined) return;
+
+			await this.processWeatherRequest();
+			const finishedWeatherRequest = await this.weatherInfo;
+
+			this.displayValuesOfRequest(finishedWeatherRequest);
+
 		},
 
 		async processWeatherRequest(){
 			if (this.resultInfoCountry === null) return;
+
 			const idOfCountry = this.resultInfoCountry.locations[0].id;
 
-			const options = {
-				method: 'GET',
-				headers: {
-					'X-RapidAPI-Key': this.$refs.rapidAPIKey.value,
-					'X-RapidAPI-Host': _weatherApi,
-				}
-			};
+			const options = this.optionsForApi();
 
-			return await fetch(`${URL}/current/${idOfCountry}?alt=0&tempunit=C&windunit=MS&tz=Europe%2FLondon&lang=en`, options)
-				.then(res => res.json())
-				.then(res => console.log(res));
-		}
-	},
-	async mounted() {
-		try {
-			this.$refs.temperature.innerText = this.weatherInformation.current.temperature;
-			this.$refs.symbolPhrase.innerText = this.weatherInformation.current.symbolPhrase;
-		} catch (error) {
-			this.$refs.temperature.innerText = "Sorry, we couldn't request the information";
-			this.$refs.symbolPhrase.innerText = "Sorry, we couldn't request the information";
-		}		
+			const fetchedWeatherInfo = await fetch(`${URL}/current/${idOfCountry}?alt=0&tempunit=C&windunit=MS&tz=Europe%2FLondon&lang=en`, options);
+			this.weatherInfo = fetchedWeatherInfo.json();
+		},
+
+		displayValuesOfRequest(finishedWeatherRequest){
+			this.$refs.temperature.innerHTML = "The temperature is " + finishedWeatherRequest.current.temperature + " °C";
+			this.$refs.symbolPhrase.innerHTML = "This weather is " + finishedWeatherRequest.current.symbolPhrase;
+		},
 	},
 });
 
