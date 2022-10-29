f4a2e08ac100b7ec7caad7f582e3ca00ef79be04
denis.topallaj <denis.topallaj13@gmail.com>
2022-10-29 09:51:30 +0200

    refs.temperature and symbolPhrase are not recognised, will fix later

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 /* 90afc290a4msh942c639bb0fe563p15afe0jsn7d560927cf10 */
 const _firstCountry = 0;
-const URL = "https://foreca-weather.p.rapidapi.com/location/search/";
+const URL = "https://foreca-weather.p.rapidapi.com";
 const _weatherApi = "foreca-weather.p.rapidapi.com";
 
 const _app = Vue.createApp({
@@ -28,9 +28,9 @@ const _app = Vue.createApp({
 				}
 			};
 			
-			this.resultInfoCountry = await fetch(`${URL}${this.$refs.city.value}?lang=en&country=${this.currentCode.toLowerCase()}`, options)
+			this.resultInfoCountry = await fetch(`${URL}/location/search/${this.$refs.city.value}?lang=en&country=${this.currentCode.toLowerCase()}`, options)
 			.then(res => res.json());
-			await this.processWeatherRequest();
+			this.weatherInformation = await this.processWeatherRequest();
 		},
 
 		async processWeatherRequest(){
@@ -45,18 +45,19 @@ const _app = Vue.createApp({
 				}
 			};
 
-			this.weatherInformation = await fetch(`https://foreca-weather.p.rapidapi.com/current/${idOfCountry}?alt=0&tempunit=C&windunit=MS&tz=Europe%2FLondon&lang=en`, options)
-				.then(response => response.json())
-				.then(response => console.log(response))
-				.catch(err => console.error(err));
-
-			console.log(this.weatherInformation.current);
-/*
-			this.$refs.temperature.innerHTML = `Temperature: ${this.weatherInformation.current.temperature} ${this.weatherInformation.current.tempunit}`;
-			this.$refs.weather.innerHTML = `Weather: ${this.weatherInformation.current.symbolPhrase}`;
-			*/
-
+			return await fetch(`${URL}/current/${idOfCountry}?alt=0&tempunit=C&windunit=MS&tz=Europe%2FLondon&lang=en`, options)
+				.then(res => res.json())
+				.then(res => console.log(res));
 		}
+	},
+	async mounted() {
+		try {
+			this.$refs.temperature.innerText = this.weatherInformation.current.temperature;
+			this.$refs.symbolPhrase.innerText = this.weatherInformation.current.symbolPhrase;
+		} catch (error) {
+			this.$refs.temperature.innerText = "Sorry, we couldn't request the information";
+			this.$refs.symbolPhrase.innerText = "Sorry, we couldn't request the information";
+		}		
 	},
 });
 
